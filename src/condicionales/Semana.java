e6007a6b5159e9b975fc6d682625d379aeb124bd
maryli891 <marytta095@gmail.com>
2017-11-03 20:40:22 -0300

    se realizo clase Semana con switch

--- src/condicionales/Semana.java
+++ src/condicionales/Semana.java
@@ -5,10 +5,51 @@
  */
 package condicionales;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author LC22
  */
-public class Semana {
+public class Semana
+{
+ public static void main(String[] args)   
+ {
+  int numero= Integer.parseInt(JOptionPane.showInputDialog(" Ingrese un numero entre 1 y 7")); 
+
+switch(numero) //Se recibe un valor
+{
+        case 1:
+        JOptionPane.showMessageDialog(null," LUNES");
+        break;
+        
+        case 2:
+        JOptionPane.showMessageDialog(null," MARTES ");
+        break;
+        
+       case 3:
+        JOptionPane.showMessageDialog(null," MIERCOLES ");
+        break;
+        
+        case 4:
+        JOptionPane.showMessageDialog(null," JUEVES ");
+        break;
+       
+        case 5:
+            
+        JOptionPane.showMessageDialog(null," VIERNES");
+        break;
+        
+         case 6:
+            
+        JOptionPane.showMessageDialog(null," VIERNES");
+        break;
+         case 7:
+            
+        JOptionPane.showMessageDialog(null," VIERNES");
+        break;
+        
+}
+} 
     
 }
