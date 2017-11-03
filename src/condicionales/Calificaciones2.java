670d8db47386c364439d2db96aa50f3c555c5de5
maryli891 <marytta095@gmail.com>
2017-11-03 19:17:58 -0300

    se ejecuto el ejercicio calificaciones2

--- src/condicionales/Calificaciones2.java
+++ src/condicionales/Calificaciones2.java
@@ -5,10 +5,51 @@
  */
 package condicionales;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author LC22
  */
-public class Calificaciones2 {
+public class Calificaciones2 
+{
+ 
+    public static void main(String[] args) 
+    {
+     float nota;
+     
+     nota= Float.parseFloat(JOptionPane.showInputDialog(" Ingrese la nota "));
+     
+     
+     
+        if (nota==7) 
+        
+        {
+            
+            JOptionPane.showMessageDialog(null," Notable ");
+        }
+       else
+         if (nota>=6 && nota<=7) 
+        {
+              
+           JOptionPane.showMessageDialog(null," Bien");        
+        }
+         else
+        if (nota >=4 && nota<=5)
+        {
+          
+
+           JOptionPane.showMessageDialog(null," Suficiente");
+        } 
+        else
+         if (nota >=0 && nota<=4)
+        {
+          
+           JOptionPane.showMessageDialog(null," Insuficiente");
+        } 
+        
+     } 
+
     
+
 }
